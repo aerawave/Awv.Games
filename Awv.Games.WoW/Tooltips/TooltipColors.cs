b714c91633b3b4bcf578688d52677ca0d154bee5
Joshua <jaxkinzer@aerawave.com>
2020-02-17 21:36:15 -0500

    Moved some tooltip colors as well as commented out one that seemed to be wrong... Not sure where I got it.

--- Awv.Games.WoW/Tooltips/TooltipColors.cs
+++ Awv.Games.WoW/Tooltips/TooltipColors.cs
@@ -14,6 +14,19 @@ namespace Awv.Games.WoW.Tooltips
         public static readonly Rgba32 Rare = new Rgba32(0, 112, 221);
         public static readonly Rgba32 Epic = new Rgba32(147, 69, 255);
         public static readonly Rgba32 Legendary = new Rgba32(255, 128, 0);
+        public static readonly Rgba32 Artifact = new Rgba32(229, 204, 128);
+        public static readonly Rgba32 Heirloom = new Rgba32(0, 204, 255);
+        #endregion
+        #region Other
+        //public static readonly Rgba32 Artifact = new Rgba32(153, 136, 86); // i don't know where i got this color??
+        public static readonly Rgba32 Token = new Rgba32(0, 112, 144);
+        public static readonly Rgba32 Flavor = new Rgba32(255, 209, 0);
+        public static readonly Rgba32 ItemType = new Rgba32(102, 187, 255);
+        public static readonly Rgba32 Corruption = new Rgba32(132, 97, 185);
+        public static readonly Rgba32 Blue = ItemType;
+        public static readonly Rgba32 White = Common;
+        public static readonly Rgba32 Lime = Uncommon;
+        public static readonly Rgba32 Yellow = Flavor;
         #endregion
         #region Stats
         public static readonly Rgba32 PrimaryStat = Common;
@@ -29,17 +42,6 @@ namespace Awv.Games.WoW.Tooltips
         public static readonly Rgba32 FillDefault = new Rgba32(1, 6, 26);
         public static readonly Rgba32 FillCorrupted = new Rgba32(13, 13, 26);
         public static readonly Rgba32 FillAzerite = new Rgba32(15, 20, 28);
-        #endregion
-        #region Other
-        public static readonly Rgba32 Artifact = new Rgba32(153, 136, 86);
-        public static readonly Rgba32 Token = new Rgba32(0, 112, 144);
-        public static readonly Rgba32 Flavor = new Rgba32(255, 209, 0);
-        public static readonly Rgba32 ItemType = new Rgba32(102, 187, 255);
-        public static readonly Rgba32 Corruption = new Rgba32(132, 97, 185);
-        public static readonly Rgba32 Blue = ItemType;
-        public static readonly Rgba32 White = Common;
-        public static readonly Rgba32 Lime = Uncommon;
-        public static readonly Rgba32 Yellow = Flavor;
         #endregion
         #endregion
         #region Converter extensions
@@ -69,6 +71,5 @@ namespace Awv.Games.WoW.Tooltips
             return Common;
         }
         #endregion
-
     }
 }
