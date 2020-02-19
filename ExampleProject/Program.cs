44061d26cbba2a38ff94f9af420f03bf29ab80d7
Joshua <jaxkinzer@aerawave.com>
2020-02-19 02:10:38 -0500

    Added some clarifying alerts when example info is not provided, as well as added the TooltipGenerators class.

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -3,6 +3,7 @@ using Awv.Games.WoW.Data.Artwork;
 using Awv.Games.WoW.Data.Items;
 using System;
 using System.IO;
+using System.Text;
 
 namespace ExampleProject
 {
@@ -10,17 +11,20 @@ namespace ExampleProject
     {
         public static void Main(string[] args)
         {
-            ExportItemData();
+            CustomWoWItems();
         }
 
         /// <summary>
         /// An example of how to use the <see cref="ArtworkUpdater"/>.
         /// 
         /// This will run through all files in a given directory (the "blpDirectory" variable below) and convert every *.blp file to a *.png file, and save it to the output directory (the "pngDirectory" variable below).
+        /// 
+        /// If you're not sure how to get the BLP files / directory, please see https://github.com/aerawave/Awv.Games/blob/master/Awv.Games.WoW/exporting-blizzard-interface-art.md for a guide on how to do this.
         /// </summary>
         static void UpdateArtwork()
         {
             string WoWDirectory = null;
+            Alert.Check(WoWDirectory, nameof(WoWDirectory), $"the {nameof(UpdateArtwork)} method", @"This directory will likely be at 'C:\Program Files (x86)\World of Warcraft' on Windows.");
             var blpDirectory = Path.Combine(WoWDirectory, "BlizzardInterfaceArt");
             var pngDirectory = Path.Combine(Directory.GetCurrentDirectory(), "BlizzardInterfaceArt");
             var updater = new ArtworkUpdater("updater.json", blpDirectory, pngDirectory);
@@ -69,7 +73,9 @@ namespace ExampleProject
         static void ExportItemData()
         {
             // Takes some time....
-            var database = CreateItemDatabase(@"\\awvserv\Apps\Data\Exported (wow.tools)");
+            string databaseDirectory = null;
+
+            var database = CreateItemDatabase(databaseDirectory);
             var weaponTypesPath = Path.Combine(Directory.GetCurrentDirectory(), "export/weapon-types.json");
             var iconUsageDirectory = Path.Combine(Directory.GetCurrentDirectory(), "export/Icon Usage");
             var format = ExportFormat.PlainText;
@@ -81,5 +87,25 @@ namespace ExampleProject
             ExportWeaponIconUsageData(database, iconUsageDirectory, format);
         }
 
+        static void CustomWoWItems()
+        {
+
+        }
+
+    }
+
+    public static class Alert
+    {
+        public static void Check(string value, string variableName, string location, string resolutionAdvice)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                var alert = new StringBuilder();
+                alert.AppendLine($"Have you set the {variableName} variable in {location}?");
+                alert.AppendLine("This is required.");
+                alert.AppendLine(resolutionAdvice);
+                throw new Exception(alert.ToString());
+            }
+        }
     }
 }
---
+++ ExampleProject/TooltipGenerators.cs
@@ -0,0 +1,67 @@
+﻿using Awv.Games.WoW.Graphics;
+using Awv.Games.WoW.Items;
+using Awv.Games.WoW.Tooltips;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+using System.IO;
+
+namespace ExampleProject
+{
+    public static class TooltipGenerators
+    {
+        public static readonly TooltipGenerator DefaultGenerator = new TooltipGenerator();
+        public static readonly TooltipGenerator CorruptedGenerator = new TooltipGenerator();
+        public static readonly TooltipGenerator AzeriteGenerator = new TooltipGenerator();
+
+        public static TooltipGenerator Resolve(IItem item)
+        {
+            if (item.IsCorrupted()) return CorruptedGenerator;
+            else return DefaultGenerator;
+        }
+
+        static TooltipGenerators()
+        {
+            string blizzardInterfaceArtDirectory = null;
+            Alert.Check(blizzardInterfaceArtDirectory, nameof(blizzardInterfaceArtDirectory), $"the static {nameof(TooltipGenerator)} constructor", "This directory will be the root directory for where your exported PNG files are that were converted from BLP files.");
+
+            var fillFolder = Path.Combine(Directory.GetCurrentDirectory(), @"Assets\Tooltips");
+            var borderFolder = Path.Combine(blizzardInterfaceArtDirectory, @"\Interface\Tooltips");
+            var corruptedItemsFolder = Path.Combine(blizzardInterfaceArtDirectory, @"\Interface\CorruptedItems");
+            var azeriteItemsFolder = Path.Combine(blizzardInterfaceArtDirectory, @"\Interface\Azerite");
+            var currencyFolder = Path.Combine(blizzardInterfaceArtDirectory, @"\Interface\MONEYFRAME");
+            var currencyName = "UI-MoneyIcons.png";
+            var corruptedEmblemName = "CorruptedTooltip.png";
+            var azeriteEmblemName = "AzeriteTooltip.png";
+
+            var currencyPath = Path.Combine(currencyFolder, currencyName);
+
+            DefaultGenerator.Border.Load(Path.Combine(borderFolder, "UI-Tooltip-Border.png"), 16);
+            DefaultGenerator.Fill.Load(Path.Combine(fillFolder, "Fill-Default.png"), 16);
+            DefaultGenerator.FillColor = TooltipColors.FillDefault;
+            DefaultGenerator.Currency.Load(currencyPath, 16);
+
+            CorruptedGenerator.Border.Load(Path.Combine(borderFolder, "UI-Tooltip-Border-Corrupted.png"), 32);
+            CorruptedGenerator.Fill.Load(Path.Combine(fillFolder, "Fill-Corrupt.png"), 32);
+            CorruptedGenerator.FillColor = TooltipColors.FillCorrupted;
+            CorruptedGenerator.Currency.Load(currencyPath, 16);
+
+            var corruptedEmblem = Image.Load<Rgba32>(Path.Combine(corruptedItemsFolder, corruptedEmblemName));
+            corruptedEmblem.Mutate(x => x.Crop(new Rectangle(12, 0, 76, 29)));
+
+            CorruptedGenerator.Emblem = corruptedEmblem;
+            CorruptedGenerator.EmblemAnchorY = 10;
+
+            AzeriteGenerator.Border.Load(Path.Combine(borderFolder, "UI-Tooltip-Border-Azerite.png"), 32);
+            AzeriteGenerator.Fill.Load(Path.Combine(fillFolder, "Fill-Azerite.png"), 32);
+            AzeriteGenerator.FillColor = TooltipColors.FillAzerite;
+            AzeriteGenerator.Currency.Load(currencyPath, 16);
+
+            var azeriteEmblem = Image.Load<Rgba32>(Path.Combine(azeriteItemsFolder, azeriteEmblemName));
+
+            AzeriteGenerator.Emblem = azeriteEmblem;
+            AzeriteGenerator.EmblemAnchorY = 11;
+        }
+    }
+}
